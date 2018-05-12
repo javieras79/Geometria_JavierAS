d2073668a9a7d2fc2f9518a07183a00d42d9b405
javieras79 <javieras79@gmail.com>
2018-05-12 20:21:53 +0200

    versión comentada

--- geometria/src/Rectangulo_JAS.java
+++ geometria/src/Rectangulo_JAS.java
@@ -1,5 +1,9 @@
 package geometria.src;
-
+/**
+ * 
+ * @author Javier Aznar Serna
+ * @version 1.1
+ */
 public class Rectangulo_JAS extends FiguraGeometrica_JAS {
 	private double l1;
 	private double l2;
@@ -9,12 +13,22 @@ public class Rectangulo_JAS extends FiguraGeometrica_JAS {
 		l1 = lG;
 		l2 = lP;
 	}
-
+	/**
+	 * Método que multiplica lado1 y lado2
+	 * @param l1
+	 * @param l2
+	 * @return double
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+	/**
+	 * Método que multiplica lado1 y lado2 por dos y se suman ambos resultados
+	 * @param l1
+	 * @param l2
+	 * @return double	
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
